943b6a3b5d4c344ff6f099fcd66194de8ee40b61
MELANY CHACÓN GARCÍA <49611204@ceibal.edu.uy>
2026-05-29 18:37:21 -0300

    hola

--- main.cpp
+++ main.cpp
@@ -32,6 +32,7 @@ struct Juego {
 Jugador crear_jugador();
 void menu();
 void imprimirJugador();
+bool fecha_valida();
 
 
 // ************************************************************************
@@ -40,7 +41,7 @@ int main () {
     menu();
     Jugador jugador;
     crear_jugador();
-    imprimirJugador(jugador); 
+    imprimirJugador(); 
   
     return 0;
 }
@@ -70,7 +71,7 @@ Jugador crear_jugador(){
     system("clear");
 
     cout << "Fecha de nacimiento:" << endl << " Mes >>";
-    getline(cin, j.fecha_nacimiento.mes); 
+    getline (cin, j.fecha_nacimiento.mes); 
     system("clear");
     
     cout << "Fecha de nacimiento:" << endl << " Año >>";
@@ -94,8 +95,8 @@ void menu() {
 };
 
 // IMPRIMIR DATOS DE UN JUGADOR
-void imprimirJugador(Jugador j) {
-  
+void imprimirJugador() {
+    Jugador j;
     cout << "Alias: " << j.alias << endl;
     cout << "Nombre: " <<j.nombre << endl;
     cout << "Apellido: " << j.apellido << endl;
@@ -103,3 +104,42 @@ void imprimirJugador(Jugador j) {
     cout << "Mes:" << j.fecha_nacimiento.mes << endl;
     cout << "Año:" << j.fecha_nacimiento.anio << endl;
 };
+
+bool fecha_valida() {
+    Fecha f;
+    // 1. Validar el año (mayor o igual a 2000)
+    if (f.anio < 2000) {
+        return false; 
+    }
+
+    // 2. Validar que el día no sea negativo o cero
+    if (f.dia < 1) {
+        return false;
+    }
+
+    // 3. Determinar el límite de días según el mes escrito por el usuario
+    int max_dias;
+// No tuvimos en cuenta el año bisciesto ¡¡te queremos Nico!!
+    if (f.mes == "2" || f.mes == "02") {
+            max_dias = 28;
+    } 
+    // Meses con 30 días
+    else if (f.mes == "4" || f.mes == "04" || f.mes == "6" || f.mes == "06" || f.mes == "9" || f.mes == "09" || f.mes == "11") {
+        max_dias = 30;
+    } 
+    //Meses con 31 días
+    else if (f.mes == "1" || f.mes == "01" || f.mes == "3" || f.mes == "03" || f.mes == "5" || f.mes == "05" || f.mes == "7" || f.mes == "07" || f.mes == "8" || f.mes == "08" || f.mes == "10" || f.mes == "12"){
+        max_dias = 31;
+    }
+    //Si se ingresa algo diferente de los números esperados. 
+    else {
+        cout << "Por favor, ingrese un mes válido." << endl;
+    }
+   
+    //Si el día ingresado supera el máximo. 
+    if (f.dia > max_dias) {
+        return false;
+    }
+
+    return true; // Si pasó todos los filtros, la fecha es válida
+}
