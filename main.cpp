6a578cd02b8632ff4750dd9e20e767bbd33d645b
MELANY CHACÓN GARCÍA <49611204@ceibal.edu.uy>
2026-05-29 16:18:49 -0300

    hola

--- main.cpp
+++ main.cpp
@@ -4,7 +4,35 @@ using namespace std;
 /* JUEGO SIMÓN */
 
 // ****************** Estructuras *********************
-struct Menu{
-  void imprmir_menu()
 
-};
+
+
+
+// ****************************************************
+
+
+// ****************** Prototipos de Funciones y Procedimientos ************
+
+
+
+void menu() {
+    cout << "Bienvenidos al Juego SIMON." << endl;
+    cout << "Ingrese una de las siguientes opciones: " << endl;
+    cout << "   1. Gestionar jugadores" << endl;
+    cout << "   2. Jugar" << endl;
+    cout << "   3. Informes" << endl;
+    cout << "   4. Salir" << endl;
+    cout << ">> ";
+}
+
+
+
+// ************************************************************************
+
+int main () {
+    return 0;
+
+
+}
+
+
