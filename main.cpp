74a3fa6a2a9a755d860a39a5d23be7fc26813abc
MELANY CHACÓN GARCÍA <49611204@ceibal.edu.uy>
2026-05-29 16:55:11 -0300

    hola

--- main.cpp
+++ main.cpp
@@ -10,13 +10,13 @@ using namespace std;
 struct Fecha{
     int dia;
     string mes;
-    int año;
+    int anio;
 };
 struct Jugador{
     string alias;
     string nombre;
     string apellido;
-    struct Fecha;
+    Fecha fecha_nacimiento;
     bool estado;
 };    
 
@@ -28,6 +28,28 @@ Jugador jugadores[MAX_JUGADORES];
 
 // ****************** Prototipos de Funciones y Procedimientos ************
 
+Jugador crear_jugador(Jugador &j){
+    system("clear");
+    cin.ignore();
+    cout << "Ingrese su alias: ";
+    getline(cin, j.alias); 
+    system("clear");
+    cout << "Ingrese nombres: ";
+    getline(cin, j.nombre); 
+    system("clear");
+    cout << "Ingrese apellido: ";
+    getline (cin, j.apellido);
+    system("clear");
+    cin.ignore();    
+    cout << "Fecha de nacimiento:" << endl << " Día >>";
+    cin >> j.fecha_nacimiento.dia; 
+    cout << "Fecha de nacimiento:" << endl << " Mes >>";
+    getline(cin, j.fecha_nacimiento.mes); 
+    cout << "Fecha de nacimiento:" << endl << " Año >>";
+    cin >> j.fecha_nacimiento.anio; 
+    system("clear");
+    return j;
+}
 
 
 void menu() {
@@ -46,8 +68,15 @@ void menu() {
 
 int main () {
     return 0;
+    Jugador jugador;
 
+    crear_jugador(jugador);
 
+    cout << "Datos: " << endl;
+    cout << "Alias: " << jugador.alias << endl;
+    cout << "Nombre: " << jugador.nombre << endl;
+    cout << "Apellido: " << jugador.apellido << endl;
+    cout << "Fecha: " << jugador.fecha_nacimiento.dia << " de " << jugador.fecha_nacimiento.mes << " del año " << jugador.fecha_nacimiento.anio << endl;
 }
 
 
